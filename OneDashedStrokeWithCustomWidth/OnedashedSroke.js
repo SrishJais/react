b8a1901436a36fd4560fb3186d5481d6ee807a01
Srishti Jaiswal <84665176+SrishJais@users.noreply.github.com>
2024-01-12 18:12:37 +0530

    Add files via upload

---
+++ OneDashedStrokeWithCustomWidth/OnedashedSroke.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const OnedashedSroke = () => {
+  return (
+    <div
+    style={{
+      border: "2px solid rgb(224, 224, 224)",
+      width: "65px",
+      borderRadius: "5px",
+    }}
+    className='mr-1'
+  ></div>
+  )
+}
+
+export default OnedashedSroke;
